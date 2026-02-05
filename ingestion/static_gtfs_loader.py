b75aefd7d1d00eb55d47198ca059537846841f92
jaywantpatel <jaywantpatel467@gmail.com>
2026-02-05 00:41:49 -0500

    Added auto download of gtfs static file and fixed gitignore

--- ingestion/static_gtfs_loader.py
+++ ingestion/static_gtfs_loader.py
@@ -1,6 +1,18 @@
 import zipfile
 import pandas as pd
+import requests
+import os
 from sqlalchemy import create_engine
+
+GTFS_URL = "https://oct-gtfs-emasagcnfmcgeham.z01.azurefd.net/public-access/GTFSExport.zip"
+GTFS_FILE = "GTFSExport.zip"
+
+if not os.path.exists(GTFS_FILE):
+    print("Downloading GTFS data...")
+    r = requests.get(GTFS_URL)
+    with open(GTFS_FILE, 'wb') as f:
+        f.write(r.content)
+    print("Download complete.")
 
 engine = create_engine("postgresql://transit:transit@localhost:5432/transitdb")
 
